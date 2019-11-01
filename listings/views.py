8bbcd860ac213a73cdd48006bc8985d416f5dad0
Karthik Achanta <personal4kartheek@gmail.com>
2019-10-31 21:29:03 -0400

    Committing all the changes

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    # second parameter indicates how many per page we want
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
